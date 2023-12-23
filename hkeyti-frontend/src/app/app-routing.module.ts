068f88013225fb248a56bab99acc5aa619fd7949
Mohamed Ben Othman <Mohamedbenothman2@outlook.com>
2023-12-23 15:04:26 +0100

    Added the frontend base elments

--- hkeyti-frontend/src/app/app-routing.module.ts
+++ hkeyti-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostCreationComponent } from './pages/post-creation/post-creation.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
 const routes: Routes = [
-  { path: "create-post", component: PostCreationComponent }
+  { path: "hkeyti", component: MainLayoutComponent, children: [
+      { path: "create-post", component: PostCreationComponent }
+    ] 
+  }
 ];
 
 @NgModule({
--- hkeyti-frontend/src/app/app.module.ts
+++ hkeyti-frontend/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreationComponent } from './pages/post-creation/post-creation.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreationComponent
+    PostCreationComponent,
+    NavbarComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
